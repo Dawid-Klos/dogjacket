bcfe9e2fe1d38e5fa9d23eea6129ea37d2baa5ec
Dawid Klos <klosdawid12@gmail.com>
2020-11-05 15:02:22 +0000

    Finished Back and Front panel. Added the icons for switch on and of the panel. And also the color picker.

--- src/js/index.js
+++ src/js/index.js
@@ -13,18 +13,19 @@ console.log('HELLO 🚀')
 
 // Menu links //
 const nav = document.querySelector(".nav--js");
-const menuSubpage = document.querySelector('.nav__link--stats');
+const menuFront = document.querySelector('.nav__link--stats');
 const menuSubpage2 = document.querySelector('.nav__link--settings');
 const menuBacktoHome = document.querySelector('.nav__link--home');
 
 // Subpages //
-const subpage = document.querySelector('.subpage--js');
+const front = document.querySelector('.front--js');
 const subpage2 = document.querySelector('.subpage2--js');
 const hamburger = document.querySelector(".hamburger--js");
 
-//  Color  //
+//  Color - Back panels  //
 
-const changeColor = document.querySelector('.square-save--js');
+const squareOn = document.querySelector('.square-on--js');
+const squareOff = document.querySelector('.square-off--js');
 
 const square1 = document.querySelector('.square1--js');
 const square2 = document.querySelector('.square2--js');
@@ -51,6 +52,36 @@ const square22 = document.querySelector('.square22--js');
 const square23 = document.querySelector('.square23--js');
 const square24 = document.querySelector('.square24--js');
 
+//  Color - Front panels  //
+
+const frontOn = document.querySelector('.front-on--js');
+const frontOff = document.querySelector('.front-off--js');
+
+const frontSquare1 = document.querySelector('.front-square1--js');
+const frontSquare2 = document.querySelector('.front-square2--js');
+const frontSquare3 = document.querySelector('.front-square3--js');
+const frontSquare4 = document.querySelector('.front-square4--js');
+const frontSquare5 = document.querySelector('.front-square5--js');
+const frontSquare6 = document.querySelector('.front-square6--js');
+const frontSquare7 = document.querySelector('.front-square7--js');
+const frontSquare8 = document.querySelector('.front-square8--js');
+const frontSquare9 = document.querySelector('.front-square9--js');
+const frontSquare10 = document.querySelector('.front-square10--js');
+const frontSquare11 = document.querySelector('.front-square11--js');
+const frontSquare12 = document.querySelector('.front-square12--js');
+const frontSquare13 = document.querySelector('.front-square13--js');
+const frontSquare14 = document.querySelector('.front-square14--js');
+const frontSquare15 = document.querySelector('.front-square15--js');
+const frontSquare16 = document.querySelector('.front-square16--js');
+const frontSquare17 = document.querySelector('.front-square17--js');
+const frontSquare18 = document.querySelector('.front-square18--js');
+const frontSquare19 = document.querySelector('.front-square19--js');
+const frontSquare20 = document.querySelector('.front-square20--js');
+const frontSquare21 = document.querySelector('.front-square21--js');
+const frontSquare22 = document.querySelector('.front-square22--js');
+const frontSquare23 = document.querySelector('.front-square23--js');
+const frontSquare24 = document.querySelector('.front-square24--js');
+
 // HAMBURGER MENU //
 
 hamburger.addEventListener('click', function (event) {
@@ -63,7 +94,7 @@ hamburger.addEventListener('click', function (event) {
 
 // button //
 menuBacktoHome.addEventListener('click', function (event) {
-    subpage.classList.remove('subpage--open');
+    front.classList.remove('front--open');
     subpage2.classList.remove('subpage2--open');
     nav.classList.toggle('nav--open');
     hamburger.classList.toggle('is-active');
@@ -73,8 +104,8 @@ menuBacktoHome.addEventListener('click', function (event) {
 // Subpage//
 
 // button //
-menuSubpage.addEventListener('click', function (event) {
-    subpage.classList.add('subpage--open');
+menuFront.addEventListener('click', function (event) {
+    front.classList.add('front--open');
     subpage2.classList.remove('subpage2--open');
     nav.classList.toggle('nav--open');
     hamburger.classList.toggle('is-active');
@@ -87,17 +118,17 @@ menuSubpage.addEventListener('click', function (event) {
 // button //
 menuSubpage2.addEventListener('click', function (event) {
     subpage2.classList.add('subpage2--open');
-    subpage.classList.remove('subpage--open');
+    front.classList.remove('front--open');
     nav.classList.toggle('nav--open');
     hamburger.classList.toggle('is-active');
 });
 
 
+// Back panel - Changing color //
 
+squareOn.addEventListener('click', function(event) {
+  
 
-// Changing color //
-
-changeColor.addEventListener('click', function(event) {
    square1.style.backgroundColor = '#fff';
    square2.style.backgroundColor = '#fff';
    square3.style.backgroundColor = '#fff';
@@ -122,10 +153,80 @@ changeColor.addEventListener('click', function(event) {
    square22.style.backgroundColor = '#fff';
    square23.style.backgroundColor = '#fff';
    square24.style.backgroundColor = '#fff';
+
 });
 
 
-// user input - changing color pallette //
+squareOff.addEventListener('click', function(event) {
+  
+
+  square1.style.backgroundColor = '#6b5f5f';
+  square2.style.backgroundColor = '#6b5f5f';
+  square3.style.backgroundColor = '#6b5f5f';
+  square4.style.backgroundColor = '#6b5f5f';
+  square5.style.backgroundColor = '#6b5f5f';
+  square6.style.backgroundColor = '#6b5f5f';
+  square7.style.backgroundColor = '#6b5f5f';
+  square8.style.backgroundColor = '#6b5f5f';
+  square9.style.backgroundColor = '#6b5f5f';
+  square10.style.backgroundColor = '#6b5f5f';
+  square11.style.backgroundColor = '#6b5f5f';
+  square12.style.backgroundColor = '#6b5f5f';
+  square13.style.backgroundColor = '#6b5f5f';
+  square14.style.backgroundColor = '#6b5f5f';
+  square15.style.backgroundColor = '#6b5f5f';
+  square16.style.backgroundColor = '#6b5f5f';
+  square17.style.backgroundColor = '#6b5f5f';
+  square18.style.backgroundColor = '#6b5f5f';
+  square19.style.backgroundColor = '#6b5f5f';
+  square20.style.backgroundColor = '#6b5f5f';
+  square21.style.backgroundColor = '#6b5f5f';
+  square22.style.backgroundColor = '#6b5f5f';
+  square23.style.backgroundColor = '#6b5f5f';
+  square24.style.backgroundColor = '#6b5f5f';
+
+});
+
+// Front panel - changing color //
+
+frontOn.addEventListener('click', function(event) {
+  
+
+  frontSquare1.style.backgroundColor = '#fff';
+  frontSquare2.style.backgroundColor = '#fff';
+  frontSquare3.style.backgroundColor = '#fff';
+  frontSquare4.style.backgroundColor = '#fff';
+  frontSquare5.style.backgroundColor = '#fff';
+  frontSquare6.style.backgroundColor = '#fff';
+  frontSquare7.style.backgroundColor = '#fff';
+  frontSquare8.style.backgroundColor = '#fff';
+  frontSquare9.style.backgroundColor = '#fff';
+  frontSquare10.style.backgroundColor = '#fff';
+
+
+});
+
+
+frontOff.addEventListener('click', function(event) {
+ 
+
+ frontSquare1.style.backgroundColor = '#6b5f5f';
+ frontSquare2.style.backgroundColor = '#6b5f5f';
+ frontSquare3.style.backgroundColor = '#6b5f5f';
+ frontSquare4.style.backgroundColor = '#6b5f5f';
+ frontSquare5.style.backgroundColor = '#6b5f5f';
+ frontSquare6.style.backgroundColor = '#6b5f5f';
+ frontSquare7.style.backgroundColor = '#6b5f5f';
+ frontSquare8.style.backgroundColor = '#6b5f5f';
+ frontSquare9.style.backgroundColor = '#6b5f5f';
+ frontSquare10.style.backgroundColor = '#6b5f5f';
+
+});
+
+// BACK PANEL -  user input - changing color pallette //
+
+let hamburgerSpan = document.querySelector('.hamburger-box');
+
 
 let userColor;
 let defaultColor = "#213112";
@@ -146,10 +247,44 @@ function startup() {
     if (span) {
       span.style.backgroundColor = event.target.value;
     }
+    hamburgerSpan.style.backgroundColor = '#2D2241';
   }
 
   function updateAll(event) {
     document.querySelectorAll("span").forEach(function(span) {
       span.style.backgroundColor = event.target.value;
+      hamburgerSpan.style.backgroundColor = '#2D2241';
     });
   }
+
+// FRONT PANEL -  user input - changing color pallette //
+
+let userFrontColor;
+let defaultFrontColor = "#ffffff";
+
+window.addEventListener("load", startupFront, false);
+
+function startupFront() {
+    userFrontColor = document.querySelector("#front__userColor");
+    userFrontColor.value = defaultFrontColor;
+    userFrontColor.addEventListener("input", updateFirstFront, false);
+    userFrontColor.addEventListener("change", updateAllFront, false);
+    userFrontColor.select();
+  }
+
+  function updateFirstFront(event) {
+    let spanFront = document.getElementById("front__square");
+  
+    if (spanFront) {
+      spanFront.style.backgroundColor = event.target.value;
+    }
+    hamburgerSpan.style.backgroundColor = '#2D2241';
+  }
+
+  function updateAllFront(event) {
+    document.querySelectorAll("span").forEach(function(spanFront) {
+      spanFront.style.backgroundColor = event.target.value;
+      hamburgerSpan.style.backgroundColor = '#2D2241';
+    });
+  }
+
